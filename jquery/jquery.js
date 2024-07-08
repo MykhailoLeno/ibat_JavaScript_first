eca5adfcbfcd82ff74396ba424ffadc2743d96b9
saliwanm <saliwanm@gmail.com>
2024-07-08 22:44:23 +0100

    add descriptions

--- jquery/jquery.js
+++ jquery/jquery.js
@@ -2,6 +2,7 @@
 $(document).ready(function () {
     // Ajax get
     $("#btnGetData").on("click", function () {
+        // create a request to receive data from the server through AJAX jQuery
         $.ajax({
             url: 'https://jsonplaceholder.typicode.com/posts/1',
             type: "GET",
@@ -9,6 +10,7 @@ $(document).ready(function () {
             error: errorData
         });
 
+        // a function that is called to enter the data received from the server in the paragraph with the id "result"
         function getData(data) {
             $("#result").html(
                 `<h3>${data.title}</h3>
@@ -16,6 +18,7 @@ $(document).ready(function () {
             );
         }
 
+        // function that is called when an error occurs
         function errorData(error) {
             console.error("Error:", error);
         }
@@ -25,11 +28,13 @@ $(document).ready(function () {
     $("#form").submit(function (event) {
         event.preventDefault();
 
+        // create a variable and write in it the data from the form
         const data = {
             title: $("#formTitle").val(),
             body: $("#formBody").val()
         };
 
+        // create a request to send data to the server via AJAX jQuery
         $.ajax({
             url: 'https://jsonplaceholder.typicode.com/posts',
             type: "POST",
@@ -39,8 +44,7 @@ $(document).ready(function () {
             error: errorPostData
         });
 
-        console.log(data);
-
+        // a function that records the data received from the server in a paragraph with the id="resultPost"
         function postData(data) {
             $("#resultPost").html(
                 `<h2>Post created</h2>
@@ -50,12 +54,13 @@ $(document).ready(function () {
             );
         }
 
+        // function that is called when an error occurs
         function errorPostData(error) {
             console.error("Error: ", error);
         }
     });
 
-    // Ajax JSON  
+    // Ajax JSON. Execution of a get request for automatically waiting for data in the JSON format
     $("#getJSONBtn").on("click", function () {
         $.getJSON("https://jsonplaceholder.typicode.com/users/1", function (data) {
             $("#resultJSON").html(
